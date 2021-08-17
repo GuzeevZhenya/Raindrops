a766e32706c12f55bfcd0a1c5d9cf22ebb184cc2
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-08-17 14:42:05 +0300

    create input for writting answer

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
 let firstNumber = document.querySelector('.first-number');
 let operation = document.querySelector('.operation')
 let secondNumber = document.querySelector('.second-number');
-let result = document.querySelector('.result')
+let result = document.querySelector('.result');
+let resultInput = document.querySelector('.result-input');
+let answerPlate = document.querySelector('.answer-plate');
+let count = document.querySelector('.count');
 
-function mathExample() {
-	let operations = ['*','/','+','-']
-	firstNumber.textContent = Math.floor(Math.random() * (20 + 1 - 1));
+function mathExample(min=1, max=10) {
+	let operations = ['*', '/', '+', '-'];
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	firstNumber.textContent = Math.floor(Math.random() * (max - min + 1)) + min;
 	operation.textContent = operations[Math.floor(Math.random() * operations.length)];
-	secondNumber.textContent = Math.floor(Math.random() * (20 + 1 - 1));
-
+	secondNumber.textContent = Math.floor(Math.random() * (max - min + 1)) + min;
+	
 	if (operation.textContent == '/') {
 		firstNumber.textContent = (firstNumber.textContent * secondNumber.textContent);
 	}
 
-
- 
 	switch (operation.textContent) {
 		case '+':
-			result.textContent = +firstNumber.textContent + +secondNumber.textContent;
+			result = +firstNumber.textContent + +secondNumber.textContent;
 			break;
 		case '-':
-			result.textContent = firstNumber.textContent - secondNumber.textContent;
+			result = firstNumber.textContent - secondNumber.textContent;
 			break;
 		case '*':
-			result.textContent = firstNumber.textContent * secondNumber.textContent;
+			result = firstNumber.textContent * secondNumber.textContent;
 			break;
 		case '/':
-			result.textContent = firstNumber.textContent / secondNumber.textContent;
+			result = firstNumber.textContent / secondNumber.textContent;
 			break;
 	}
 }
+mathExample(1,10)
 
-mathExample();
+resultInput.addEventListener('change', () => {
+
+	if (resultInput.value == result) {
+		count.textContent = +count.textContent + 10;
+		answerPlate.textContent = 'Правильно'
+	} else {
+		count.textContent = +count.textContent - 5;
+		answerPlate.textContent = 'Не правильно'
+	}
+	resultInput.value = '';
+	mathExample(1, 10)
+})
