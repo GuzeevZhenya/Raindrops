79889b38f5bb1cf76ae5f144fd34dd81fa8821aa
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-08-23 16:10:10 +0300

    create restart game when user lose

--- script.js
+++ script.js
@@ -5,14 +5,11 @@ let count = document.querySelector('.count-number');
 let windowMaxWidth = window.screen.availWidth - (window.outerWidth - window.innerWidth);
 let capelca = document.querySelector('.capelca')
 
-
 let raindrop = document.querySelector('.raindrop')
 let operations = ['*', '/', '+', '-'];
-
 let errors = 0;
-
 let circle = document.createElement('div');
-let circle1 = document.createElement('div')
+
 
  
 function createRandomCircle(max, min) {
@@ -79,18 +76,19 @@ function getRandomPosition(min, max) {
 	return Math.round(Math.random() * (max - min) + min)
 }
 
-function alive() {
-	setInterval( function() {
+
+
+
+let alive = setInterval(function () {
 	let wave = document.querySelector('.wave .editorial');
 	let waveHeight = wave.clientHeight;
 	let circleSize = parseInt(window.getComputedStyle(circle).getPropertyValue('top'));
 	let windowMaxHeight = window.screen.availHeight - (window.outerHeight - window.innerHeight) - waveHeight;
 	let bonusCount = 0;
-
+	let waveSize = 5;
 
 	resultInput.addEventListener('change', () => {
 		if (resultInput.value == result) {
-			console.log(result,resultInput.value)
 			bonusCount += 1;
 			resultInput.value = '';
 			count.textContent = +count.textContent + 10 + bonusCount;
@@ -101,34 +99,49 @@ function alive() {
 		} 
 	})
 	if (circleSize >= windowMaxHeight) {
-		errors += 1;
-		let waveSize = 5;
-		resultInput.value = '';
-		// wave.style.height =  waveHeight + 40 + 'px';
-		count.textContent = +count.textContent - 7 + bonusCount;
-		while (circle.firstChild) {
-			circle.removeChild(circle.firstChild);
-		}
-		createRandomCircle(1, 10);
+			errors += 1;
+			resultInput.value = '';
+			wave.style.height =  waveHeight + 40 + 'px';
+			count.textContent = +count.textContent - 7 + bonusCount;
+			while (circle.firstChild) {
+				circle.removeChild(circle.firstChild);
+			}
+			createRandomCircle(1, 10);
+			if(errors ==1){
+				gameOverBlock(count.textContent)
+				circle.remove()
+				clearInterval(alive);
+			}
 	}
-	// if (errors == 1) {
-	// 	gameOverBlock();
-	// 	window.clearInterval(stopInterval);
-	// }
 
-	})
-}
+},300)
+
+// setInterval(alive, 300);
  
-setInterval(alive,300)
+ 
+// setInterval(alive,300)
 
 
-
-
-function gameOverBlock() {
+function gameOverBlock(count) {
 	let gameOver = document.createElement('div');
 	gameOver.classList.add('game-over');
 	raindrop.append(gameOver);
 	gameOver.textContent = 'Game Over';
 	gameOver.style.display = 'flex';
+	let gameCount = document.createElement('span');
+	gameCount.textContent = 'Ваш счет: ' + count;
+	gameCount.classList.add('game-count');
+	gameOver.append(gameCount);
+	let newGameButton = document.createElement('button');
+	newGameButton.textContent = 'Новая игра';
+	gameOver.append(newGameButton);
+	startNewGame(newGameButton)
+}
+
+function startNewGame(button){
+	button.addEventListener('click',()=>{
+	//  window.location.reload() // ну тут у меня случились беды с головой,зато рабочий вариант)
+	 
+	})
 }
 
