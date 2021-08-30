abbedafe9602a93cee3cd1e810fd6dc95c20bc44
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-08-30 16:42:14 +0300

    fix bugs,remove extra code

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 let raindrop = document.querySelector('.raindrop')
-let operations = ['*', '/', '+', '-'];
+const operations = ['*', '/', '+', '-'];
 const startGameBlock = document.querySelector('.start-game');
-const gameStartBtn = document.querySelector('.start-game-btn');
+const gameStartBtn = document.querySelectorAll('.start-game-btn');
 const restartBtn = document.querySelector('.restart-btn');
 let resultInput = document.querySelector('.result-input');
 let count = document.querySelector('.count-number');
 let circle = document.createElement('div');
+const gameOver = document.querySelector('.game-over');
 let result;
-
-// startGame.style.display = 'none';
-
+let bonusCount = 0;
 const {
 	width
 } = raindrop.getBoundingClientRect()
 
-function createNewCapelca(x = 40) {
-	count.textContent = 0;
+gameOver.style.display = "none";
+
+function createNewRaindrop(x = 40) {
 	let {
 		firstNumber,
 		operation,
 		secondNumber
-	} = addCapelcaNumbers();
-
-
-	if (operation == '/') {
-		firstNumber = (firstNumber * secondNumber);
-	}
-	if (operation == '-') {
-		firstNumber = (firstNumber + secondNumber);
-	}
+	} = generateEquationNumbers();
 
 	switch (operation) {
 		case '+':
 			result = +firstNumber + +secondNumber;
 			break;
 		case '-':
+			firstNumber = (firstNumber + secondNumber);
 			result = firstNumber - secondNumber;
 			break;
 		case '*':
 			result = firstNumber * secondNumber;
 			break;
 		case '/':
+			firstNumber = (firstNumber * secondNumber);
 			result = firstNumber / secondNumber;
 			break;
 	}
@@ -55,9 +49,9 @@ function createNewCapelca(x = 40) {
 	circle.append(secondNumber)
 }
 
-function addCapelcaNumbers() {
+function generateEquationNumbers() {
 	let firstNumber = getRandomPosition(1, 10);
-	let operation = operations[Math.floor(Math.random() * operations.length)];;
+	let operation = operations[Math.floor(Math.random() * operations.length)];
 	let secondNumber = getRandomPosition(1, 10);
 	return {
 		firstNumber,
@@ -70,30 +64,26 @@ function getRandomPosition(min, max) {
 	return Math.round(Math.random() * (max - min) + min)
 }
 
-// createNewCapelca(getRandomPosition(0, width - 50));
-
 let waveSize = 200;
 let errors = 0;
+
+
+resultInput.addEventListener('change', () => {
+	if (resultInput.value == result) {
+		bonusCount += 1;
+		resultInput.value = '';
+		count.textContent = +count.textContent + 10 + bonusCount;
+		while (circle.firstChild) {
+			circle.removeChild(circle.firstChild);
+		}
+		createNewRaindrop(getRandomPosition(0, width - 50));
+	}
+})
 
 function circleLife() {
 	let circleSize = parseInt(window.getComputedStyle(circle).getPropertyValue('top'));
 	let windowMaxHeight = window.screen.availHeight - (window.outerHeight - window.innerHeight) - waveSize;
-	let bonusCount = 0;
-
-	resultInput.addEventListener('change', () => {
-		if (resultInput.value == result) {
-			bonusCount += 1;
-			resultInput.value = '';
-			count.textContent = +count.textContent + 10 + bonusCount;
-			while (circle.firstChild) {
-				circle.removeChild(circle.firstChild);
-			}
-			createNewCapelca(getRandomPosition(0, width - 50));
-		}
-	})
-console.log(circleSize,windowMaxHeight)
 	if (circleSize >= windowMaxHeight) {
-		count.textContent = 0;
 		errors += 1;
 		resultInput.value = '';
 		waveGrow();
@@ -101,16 +91,11 @@ console.log(circleSize,windowMaxHeight)
 		while (circle.firstChild) {
 			circle.removeChild(circle.firstChild);
 		}
-		// circle.remove();
-		// createNewCapelca(getRandomPosition(0, width - 50));
 		if (errors == 1) {
-			startGame(count.textContent,isLoseGame = true)
+			startGame(count.textContent, isLoseGame = true)
 		}
 	}
 }
-
-
-// setInterval(circleLife, 1000);
 
 function waveGrow() {
 	let wave = document.querySelector('.wave .editorial');
@@ -118,164 +103,35 @@ function waveGrow() {
 	wave.style.height = waveHeight + 40 + 'px';
 }
 
-gameStartBtn.addEventListener('click', startGame);
+gameStartBtn.forEach(item=>item.addEventListener('click',startGame))
+// gameStartBtn.addEventListener('click', startGame);
 
 function startGame(count, isLoseGame = false) {
-
+	console.log(1)
 	if (isLoseGame) {
-		startGameBlock.style.display = 'block';
-		let loseGameText = document.createElement('p');
-		let counter = document.createElement('p');
+		console.log(2)
+		gameOver.style.display = "block";
+		startGameBlock.style.display = "none";
+		// startGameBlock.style.display = 'block';
+		// let loseGameText = document.createElement('p');
+		// let counter = document.createElement('p');
 
-		loseGameText.textContent = 'Game Over';
-		counter.textContent = `Ваш счет ${count}`;
-		gameStartBtn.textContent = 'restart';
+		// loseGameText.textContent = 'Game Over';
+		// counter.textContent = `Ваш счет ${count}`;
+		// gameStartBtn.textContent = 'restart';
 
-		startGameBlock.prepend(counter);
+		// startGameBlock.prepend(counter);
 		errors = 0;
-		// count.textContent = 0;
-	
 		circle.style.display = 'none';
 	} else {
+		console.log(3)
+		startGameBlock.style.display = "none";
+		gameOver.style.display = "none";
+		
 		startGameBlock.removeChild(startGameBlock.firstChild);
-		startGameBlock.style.display = 'none';
 		circle.style.display = 'block';
-		createNewCapelca(getRandomPosition(0, width - 50));
-		setTimeout(circleLife, 4400);
+		createNewRaindrop(getRandomPosition(0, width - 50));
+		setInterval(circleLife, 1000);
 	}
 }
 
-
-
-// createNewCapelca(getRandomPosition(0, width - 50));
-
-// let resultInput = document.querySelector('.result-input');
-// let count = document.querySelector('.count-number');
-
-// let raindrop = document.querySelector('.raindrop')
-// let operations = ['*', '/', '+', '-'];
-// let errors = 0;
-// let circle = document.createElement('div');
-
-
-// function createRandomCircle(max, min) {
-// 	min = Math.ceil(min);
-// 	max = Math.floor(max);
-
-// 	const {
-// 		width,
-// 		height
-// 	} = raindrop.getBoundingClientRect()
-
-// 	const x = getRandomPosition(0, width - 50);
-// 	const firstNumber = document.createElement('p')
-// 	const operation = document.createElement('p')
-// 	const secondNumber = document.createElement('p')
-
-// 	firstNumber.classList.add('first-number')
-// 	operation.classList.add('operation')
-// 	secondNumber.classList.add('second-number')
-
-
-// 	firstNumber.textContent = Math.floor(Math.random() * (max - min + 1)) + min;
-// 	operation.textContent = operations[Math.floor(Math.random() * operations.length)];
-// 	secondNumber.textContent = Math.floor(Math.random() * (max - min + 1)) + min;
-
-// 	if (operation.textContent == '/') {
-// 		firstNumber.textContent = (firstNumber.textContent * secondNumber.textContent);
-// 	}
-// 	if (operation.textContent == '-') {
-// 		firstNumber.textContent = (firstNumber.textContent + secondNumber.textContent);
-// 	}
-
-// 	switch (operation.textContent) {
-// 		case '+':
-// 			result = +firstNumber.textContent + +secondNumber.textContent;
-// 			break;
-// 		case '-':
-// 			result = firstNumber.textContent - secondNumber.textContent;
-// 			break;
-// 		case '*':
-// 			result = firstNumber.textContent * secondNumber.textContent;
-// 			break;
-// 		case '/':
-// 			result = firstNumber.textContent / secondNumber.textContent;
-// 			break;
-// 	}
-
-// 	circle.classList.add('circle');
-// 	circle.style.width = `60px`;
-// 	circle.style.height = `60px`;
-// 	circle.style.left = `${x}px`
-
-// 	raindrop.append(circle);
-// 	circle.append(firstNumber)
-// 	circle.append(operation)
-// 	circle.append(secondNumber)
-// }
-
-// createRandomCircle(1, 10)
-
-
-// function getRandomPosition(min, max) {
-// 	return Math.round(Math.random() * (max - min) + min)
-// }
-
-
-// let waveSize = 160;
-
-// let alive = setInterval(function () {
-// 	let circleSize = parseInt(window.getComputedStyle(circle).getPropertyValue('top'));
-// 	let windowMaxHeight = window.screen.availHeight - (window.outerHeight - window.innerHeight)-waveSize;
-// 	let bonusCount = 0;
-
-// 	resultInput.addEventListener('change', () => {
-// 		if (resultInput.value == result) {
-// 			bonusCount += 1;
-// 			resultInput.value = '';
-// 			count.textContent = +count.textContent + 10 + bonusCount;
-// 			while (circle.firstChild) {
-// 				circle.removeChild(circle.firstChild);
-// 			}
-// 			createRandomCircle(1, 10);
-// 		} 
-// 	})
-// 	if (circleSize >= windowMaxHeight) {
-// 			errors += 1;
-// 			resultInput.value = '';
-// 			waveGrow();
-// 			count.textContent = +count.textContent - 7 + bonusCount;
-// 			while (circle.firstChild) {
-// 				circle.removeChild(circle.firstChild);
-// 			}
-// 			createRandomCircle(1, 10);
-// 			if(errors ==1){
-// 				gameOverBlock(count.textContent)
-// 				circle.style.display = 'none';
-// 				clearInterval(alive);
-// 			}
-// 	}
-
-// },1500)
-
-// function waveGrow() {
-// 	let wave = document.querySelector('.wave .editorial');
-// 	let waveHeight = wave.clientHeight;
-// 	wave.style.height =  waveHeight + 40 + 'px';
-// }
-
-
-// function gameOverBlock(count) {
-// 	let gameOver = document.querySelector('.game-over');
-// 	let newGameButton = document.querySelector('.restart-btn');
-// 	gameOver.style.display = 'block';
-// 	startNewGame(newGameButton)
-// }
-
-// function startNewGame(button){
-// 	button.addEventListener('click',()=>{
-// 	// window.location.reload() // ну тут у меня случились беды с головой,зато рабочий вариант)
-// 	circle.style.display = 'block';
-
-// 	})
-// }
