86c4596d8c5b7e783d17c917160e3a8305299830
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-09-02 09:16:23 +0300

    remove extra code

--- wave.js
+++ wave.js
@@ -3,30 +3,17 @@ class Wave {
 		this.wave = wave;
 	}
 
-	init() {
-		// this.waveGrow();
-		// this.waveReset();
-	}
-
 	waveReset() {
-	
 		this.wave.style.height = 160 + 'px';
-		console.log(this.wave.style.height)
 	}
 
 	waveGrow() {
 		let waveHeight = this.wave.clientHeight;
 		this.wave.style.height = waveHeight + 40 + 'px';
-		console.log(1)
 	}
-
 }
-
 
 let wave = document.querySelector('.wave .editorial');
 const waveClass = new Wave(wave);
 
 export default waveClass;
-// waveClass.init();
-
- 
